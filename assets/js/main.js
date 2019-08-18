733073e4b4590e14c7025524e38b4c412b06c57d
NikiProToss <nikitaklimenko.good@protonmail.com>
2019-08-18 17:34:01 +0300

    change forms styling add initial JS

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+const firstMain = document.querySelector('#main .column:nth-child(1)');
+const secondMain = document.querySelector('#main .column:nth-child(2)');
+
+firstMain.addEventListener("click", () => console.log("Hello World!"));
