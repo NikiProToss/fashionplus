c8fd2c29318acc6af8b70f67e1ffbb977e7d0a8c
NikiProToss <nikitaklimenko.good@protonmail.com>
2019-08-26 02:52:01 +0300

    Remove SW, add hero buttons

--- workbox-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "globDirectory": "./",
-  "globPatterns": [
-    "**/*.{css,js,scss,jpg,svg,ico,png,html,json,md}"
-  ],
-  "swDest": "sw.js",
-  "swSrc": "assets/js/service-worker.js"
-};
