6432ae2da549cd97df77173d646d506c6d9970da
NikiProToss <nikitaklimenko.good@protonmail.com>
2019-08-25 21:38:08 +0300

    remove service worker, leave only precaching

--- sw.js
+++ sw.js
@@ -302,31 +302,3 @@ workbox.precaching.precacheAndRoute([
     "revision": "cb9c37d1b998a5c1313cfa3e1b201433"
   }
 ]);
-
-workbox.routing.registerRoute(
-  // Cache CSS files.
-  /\.css$/,
-  // Use cache but update in the background.
-  new workbox.strategies.StaleWhileRevalidate({
-    // Use a custom cache name.
-    cacheName: 'css-cache',
-  })
-);
-
-workbox.routing.registerRoute(
-  // Cache image files.
-  /\.(?:png|jpg|jpeg|svg|gif)$/,
-  // Use the cache if it's available.
-  new workbox.strategies.CacheFirst({
-    // Use a custom cache name.
-    cacheName: 'image-cache',
-    plugins: [
-      new workbox.expiration.Plugin({
-        // Cache only 20 images.
-        maxEntries: 20,
-        // Cache for a maximum of a week.
-        maxAgeSeconds: 7 * 24 * 60 * 60,
-      })
-    ],
-  })
-);
